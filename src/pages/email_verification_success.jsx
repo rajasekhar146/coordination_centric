9c7a1439125837213fc8f9f8ff34ecaf586f293e
rajasekhar <rajasekhar.naruboina@augustahitech.com>
2021-10-22 17:42:36 +0530

    email verification pages created

---
+++ src/pages/email_verification_success.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmailVerificationSuccessComponent from '../components/SignIn/EmailVerificationSuccess.Component'
+
+const EmailVerificationSuccess = () => {
+  return (
+    <div>
+      <EmailVerificationSuccessComponent />
+    </div>
+  )
+}
+
+export default EmailVerificationSuccess
