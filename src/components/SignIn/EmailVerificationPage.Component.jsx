4fb4726198ddecbb0e86a882fe3c3806a4e9dd12
rajasekhar <rajasekhar.naruboina@augustahitech.com>
2021-10-22 16:37:16 +0530

    activate and deactivate org done

--- src/components/SignIn/EmailVerificationPage.Component.jsx
+++ src/components/SignIn/EmailVerificationPage.Component.jsx
@@ -1,14 +1,37 @@
 import React, { useState } from 'react'
-import SentIcon from '../../assets/icons/sent.png'
+import msgIcon from '../../assets/icons/msglogo.png'
 import CClogo from '../../assets/icons/cc_logo_red.png'
 import history from '../../history'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import { authenticationService } from '../../services'
 import get from 'lodash.get'
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import { makeStyles } from '@material-ui/core/styles'
 
+
+const useStyles = makeStyles(theme => ({
+    textField: {
+      width: '100%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      paddingBottom: 0,
+      marginTop: 0,
+      fontWeight: 500,
+      background: '#FFFFFF',
+      borderRadius: 8,
+      border: '1px solid #CACCCF',
+    },
+    input: {
+      color: '#838486',
+      height: '44px',
+    },
+  }))
 
 const UserVerificationPage = props => {
+    const classes = useStyles()
+    const [verificationCode, setVerificationCode] = useState('')
+
   const currentUser = authenticationService.currentUserValue
   const currentUserEmail = get(currentUser, ['data', 'data', 'email'], '')
 
@@ -19,16 +42,27 @@ const UserVerificationPage = props => {
           <img src={CClogo} alt="key" />
         </div>
         <div className="io_sent_logo">
-          <img src={SentIcon} alt="key" />
+          <img src={msgIcon} alt="key" />
         </div>
         <div className="io_error_label">
           <label>Verify Your Email Address  </label>
         </div>
         <div className="io_apologize_label io_width60">
-          <label>To continue using Coordination Center, please verify your email address:</label>
+          <label>Enter below the code that was sent to your email address:</label>
         </div>
         <div className="io_email_label">
           <label>{currentUserEmail}</label>
+        </div>
+        <div className="io__two_justify io__margin__32">
+          <TextField
+            margin="normal"
+            type="text"
+            className={classes.textField}
+            value={verificationCode}
+            onChange={e => {
+              setVerificationCode(e.target.value)
+            }}
+          />
         </div>
         <div className="io_send_emailbtn io_width90">
           <Button
---
+++ src/pages/email_verification.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmailVerificationComponent from '../components/SignIn/EmailVerificationPage.Component'
+
+const EmailVerification = () => {
+  return (
+    <div>
+      <EmailVerificationComponent />
+    </div>
+  )
+}
+
+export default EmailVerification
