29bc2b0f5de00599cfeecdde7825acdfb55f5e39
vkrishna.augusta <krishnakumar.velusamy@augustahitech.com>
2021-12-06 20:50:53 +0530

    multiple notification

--- src/App.js
+++ src/App.js
@@ -13,27 +13,47 @@ function App() {
   const [show, setShow] = useState(false);
   const [notification, setNotification] = useState({title: '', body: ''});
   const [isTokenFound, setTokenFound] = useState(false);
+  const [notificationList,setNotificationList] = useState([]);
   // getTokenFn(setTokenFound);
 
   onMessageListener().then(payload => {
+
+    let notifArray = [...notificationList];
     setShow(true);
     setTimeout(() => {
-      setShow(false);
-    }, 2000);
-    setNotification({title: payload.notification.title, body: payload.notification.body})
+      // setShow(false);
+      // setNotificationList([]);
+    }, 15000);
+    notifArray.unshift({title: payload.notification.title, body: payload.notification.body});
+    setNotificationList(notifArray);
+    // setNotification({title: payload.notification.title, body: payload.notification.body})
+
+
     eventBus.dispatch("notification", {title: payload.notification.title, body: payload.notification.body});
     console.log("12343",payload);
   }).catch(err => console.log('failed: ', err));
-
+const toDisplay =(index)=>{
+  
+  setTimeout(() => {
+    let notificationListTemp =[...notificationList];
+    notificationListTemp.splice(index,1);
+    setNotificationList(notificationListTemp);
+  }, 15000);
+  return true;
+}
   return (
     <div className="App">
   
       {/* {isTokenFound && <h1> Notification permission enabled 👍🏻 </h1>}
       {!isTokenFound && <h1> Need notification permission ❗️ </h1>} */}
   {show && (
- <div className="toast">
+      <div className="notification-popup">
+  
+    {(notificationList.map((item,index)=>{
+      if(toDisplay(index)){
+ return <div className="toast" key={item}>
         <div className="toast-header">
-          {notification.title}
+          {item.title}
           
           <button onClick={()=>{setShow(false)}} className="close">
             <CloseIcon  />
@@ -41,10 +61,11 @@ function App() {
           
         </div>
         <div className="toast-body"></div>
-        {notification.body}
+        {item.body}
 
       </div>
-
+}}))}
+</div>
 )}
       {' '}
       <Routes />
