3d9f56a4e93ccdc8fa59d763d405b7f008d906a6
Prakash Raju <prakash.raju@augustahitech.com>
2021-12-14 21:29:24 +0530

    Update Marketplace.Component.jsx

--- src/components/MarketPlace/Marketplace.Component.jsx
+++ src/components/MarketPlace/Marketplace.Component.jsx
@@ -5,25 +5,25 @@ import HighlightedDoctorsComponent from './HighlightedDoctors/HighlightedDoctors
 import './Marketplace.Component.css'
 import { appointmentService } from '../../services'
 import moment from 'moment'
-import get from 'lodash.get';
+import get from 'lodash.get'
 const MarketplaceComponent = () => {
   const [doctorsList, setDoctorsList] = useState([])
   const [searchText, setSearchText] = useState('')
   const [searchSpecialists, setSearchSpecialists] = useState([])
 
-  const [searchState,setSearchState] =useState("");
-  const [searchCity,setSearchCity] =useState("");
-  const [searchZipcode,setSearchZipcode] = useState("");
+  const [searchState, setSearchState] = useState('')
+  const [searchCity, setSearchCity] = useState('')
+  const [searchZipcode, setSearchZipcode] = useState('')
 
   useEffect(() => {
     getDoctorsList()
     console.log('Fired', searchSpecialists)
-  }, [searchText, searchSpecialists,searchState,searchCity,searchZipcode])
+  }, [searchText, searchSpecialists, searchState, searchCity, searchZipcode])
 
   const getDoctorsList = () => {
     const searchParam = {
-      pageLimit: 2000,
-      pageNo: 1,
+      pageLimit: 10,
+      pageNo: 0,
       sortByColoumn: 'name',
       sortOrder: 'desc',
       searchKey: 'name',
@@ -36,18 +36,19 @@ const MarketplaceComponent = () => {
     }
     appointmentService.getDoctorsList(searchParam).then(
       res => {
-        console.log("Get doctorslist",res);
-        if(res.status=== 200){
-          let doctors = get(res, ['data', 'data', 'list'], []);
-          const doctorsArray = [];
+        console.log('Get doctorslist', res)
+        if (res.status === 200) {
+          let doctors = res.data.data[0].totalData//get(res, ['data', 'data', 'totalData'], [])
+          console.log('doctors, ', doctors)
+          const doctorsArray = []
           doctors.forEach(doc => {
             let newObj = {
               _id: doc._id,
               name: doc.name,
               speciality: doc.speciality,
-              location: [doc.country, doc.state, doc.city].filter(n => n).join(','),
-              availability: getAvailabilityFromObject(doc.availabilities),
-              availabilityArray:doc.availabilities?.days,
+              location: doc.location, //[doc.country, doc.state, doc.city].filter(n => n).join(','),
+              availability: [], //getAvailabilityFromObject(doc.availabilities),
+              availabilityArray: [], //doc.availabilities?.days,
               pic: doc.profilePic,
               action: '',
             }
@@ -63,14 +64,14 @@ const MarketplaceComponent = () => {
     )
   }
 
-  const getAvailabilityFromObject =(availability)=>{
-    if(availability.days.length>0){
-    let oneDayAvail = availability.days[0];
-    let firstHStart = moment(oneDayAvail.first_half_starting_time).format("h:mm a");
-    let firstHEnd = moment(oneDayAvail.first_half_ending_time).format("h:mm a");
-    let SecondHStart = moment(oneDayAvail.second_half_starting_time).format("h:mm a");
-    let SecondHEnd = moment(oneDayAvail.second_half_ending_time).format("h:mm a");
-    return firstHStart +" - "+firstHEnd+ ",\n "+SecondHStart+"-"+SecondHEnd;
+  const getAvailabilityFromObject = availability => {
+    if (availability.days.length > 0) {
+      let oneDayAvail = availability.days[0]
+      let firstHStart = moment(oneDayAvail.first_half_starting_time).format('h:mm a')
+      let firstHEnd = moment(oneDayAvail.first_half_ending_time).format('h:mm a')
+      let SecondHStart = moment(oneDayAvail.second_half_starting_time).format('h:mm a')
+      let SecondHEnd = moment(oneDayAvail.second_half_ending_time).format('h:mm a')
+      return firstHStart + ' - ' + firstHEnd + ',\n ' + SecondHStart + '-' + SecondHEnd
     } else {
       return null
     }
@@ -79,19 +80,19 @@ const MarketplaceComponent = () => {
   return (
     <div className="mp__main__div">
       <div className="mp__title__text">Highlighted Doctors</div>
-      
+
       <HighlightedDoctorsComponent />
       <DoctorsearchComponent
         searchText={searchText}
         setSearchText={setSearchText}
         searchSpecialists={searchSpecialists}
         setSearchSpecialists={setSearchSpecialists}
-        searchState= {searchState}
-        setSearchState = {setSearchState}
-        searchCity ={searchCity}
-        setSearchCity = {setSearchCity}
-        searchZipcode = {searchZipcode}
-        setSearchZipcode = {setSearchZipcode}
+        searchState={searchState}
+        setSearchState={setSearchState}
+        searchCity={searchCity}
+        setSearchCity={setSearchCity}
+        searchZipcode={searchZipcode}
+        setSearchZipcode={setSearchZipcode}
       />
       <DoctorListComponent doctorsList={doctorsList} />
     </div>
